52175e9c96fbea498fca4f69bc88488389d396b4
LucyCiara <lucycht@outlook.com>
2026-06-30 22:16:46 +0200

    fix: fix libreoffice icon building by moving iconsets/ into Chicago95-theme
    This fixes the errors from the build sript and the mv command in the README. Having the iconsets directory inside the extension directory is also the standard for libreoffice extensions, so the committed change will comply with this.

--- Extras/libreoffice-chicago95-iconset/build.sh
+++ Extras/libreoffice-chicago95-iconset/build.sh
@@ -3,13 +3,12 @@
 # Goal: automate making the Chicago95 theme, for CI/CD.
 
 TOPDIR="$( dirname "$( readlink -f "${0}" )" )" # for bgstack15, /mnt/public/work/vm2/icons/git
-IMAGES_ZIP_SDIR="${TOPDIR}"/iconsets/c95
 EXT_ZIP_SDIR="${TOPDIR}"/Chicago95-theme
+IMAGES_ZIP_SDIR="${EXT_ZIP_SDIR}"/iconsets/c95
 OUTDIR="${TOPDIR}"
 
 # PROCESS
-rm -f "${OUTDIR}/images_chicago95.zip"
-cd "${IMAGES_ZIP_SDIR}" ; 7za a "${OUTDIR}/images_chicago95.zip" *
-mv -f "${OUTDIR}/images_chicago95.zip" "${EXT_ZIP_SDIR}/iconsets/"
+rm -f "${EXT_ZIP_SDIR}/iconsets/images_chicago95.zip"
+cd "${IMAGES_ZIP_SDIR}" ; 7za a "${EXT_ZIP_SDIR}/iconsets/images_chicago95.zip" *
 rm -f "${OUTDIR}/Chicago95-theme-0.0.oxt"
 cd "${EXT_ZIP_SDIR}" ; 7za a "${OUTDIR}/Chicago95-theme-0.0.oxt"
